222fa30febace1e2db4d989acbd384a65fa99393
D0683497 <ff129043923@gmail.com>
2020-04-23 20:57:10 +0800

    Add Account Register

---
+++ Komunumo/Komunumo.Admin/Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using System.Threading.Tasks;
+using Komunumo.Admin.Entities;
+using Komunumo.Admin.Models.AccountViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Komunumo.Admin.Controllers
+{
+    [Authorize]
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        #region Register
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToLocal(returnUrl);
+                }
+                AddErrors(result);
+            }
+
+            return View(model);
+        }
+
+        #endregion
+
+        #region Healpers
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Komunumo/Komunumo.Admin/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Komunumo.Admin.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "{0} 是必填的")]
+        [EmailAddress(ErrorMessage = "{0} 格式錯誤")]
+        [Display(Name = "電子郵件")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} 是必填的")]
+        [Display(Name = "使用者名稱")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "{0} 是必填的")]
+        [MinLength(4, ErrorMessage = "{0} 至少需要 4 位")]
+        [DataType(DataType.Password)]
+        [Display(Name = "密碼")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "確認密碼")]
+        [Compare("Password", ErrorMessage = "密碼不相符")]
+        public string ConfirmPassword { get; set; }
+    }
+}
