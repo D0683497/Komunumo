401494c795b4c40b65ce4db8cebf17a534c8069c
D0683497 <ff129043923@gmail.com>
2020-04-25 23:20:54 +0800

    Add Manage Index(Change Email And PhoneNumber)

---
+++ Komunumo/Komunumo.Admin/Controllers/ManageController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Komunumo.Admin.Entities;
+using Komunumo.Admin.Extensions.Alerts;
+using Komunumo.Admin.Models.ManageViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Komunumo.Admin.Controllers
+{
+    [Authorize]
+    [Route("[controller]/[action]")]
+    public class ManageController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public ManageController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        #region Index
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var model = new IndexViewModel
+            {
+                Username = user.UserName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(IndexViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var email = user.Email;
+            if (model.Email != email)
+            {
+                var setEmailResult = await _userManager.SetEmailAsync(user, model.Email);
+                if (!setEmailResult.Succeeded)
+                {
+                    throw new ApplicationException($"Unexpected error occurred setting email for user with ID '{user.Id}'.");
+                }
+            }
+
+            var phoneNumber = user.PhoneNumber;
+            if (model.PhoneNumber != phoneNumber)
+            {
+                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, model.PhoneNumber);
+                if (!setPhoneResult.Succeeded)
+                {
+                    throw new ApplicationException($"Unexpected error occurred setting phone number for user with ID '{user.Id}'.");
+                }
+            }
+
+            return RedirectToAction(nameof(Index)).WithSuccess("系統通知", "修改成功");
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> IsEmailUnique([FromQuery(Name = "Email")]string email)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user.Email == email)
+            {
+                return Json(true);
+            }
+            else if (_userManager.Users.Any(x => x.Email == email))
+            {
+                return Json(false);
+            }
+
+            return Json(true);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> IsPhoneUnique([FromQuery(Name = "PhoneNumber")]string phone)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user.PhoneNumber == phone)
+            {
+                return Json(true);
+            }
+            else if (_userManager.Users.Any(x => x.PhoneNumber == phone))
+            {
+                return Json(false);
+            }
+
+            return Json(true);
+        }
+
+        #endregion
+    }
+}
---
+++ Komunumo/Komunumo.Admin/Models/ManageViewModels/IndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Komunumo.Admin.Models.ManageViewModels
+{
+    public class IndexViewModel
+    {
+        [Display(Name = "使用者名稱")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "{0} 是必填的")]
+        [EmailAddress(ErrorMessage = "{0} 格式錯誤")]
+        [Remote("IsEmailUnique", "Manage", ErrorMessage = "{0} 已經被使用")]
+        [Display(Name = "電子郵件")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "{0} 格式錯誤")]
+        [Remote("IsPhoneUnique", "Manage", ErrorMessage = "{0} 已經被使用")]
+        [Display(Name = "手機號碼")]
+        public string PhoneNumber { get; set; }
+    }
+}
