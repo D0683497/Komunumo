c3c5d2f06861314c622df9d9a39703f5c78fb068
D0683497 <ff129043923@gmail.com>
2020-05-02 23:22:38 +0800

    Add Manage Index Page And bulma-modal-fx

---
+++ Komunumo/Komunumo.Admin/Controllers/ManageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Komunumo.Admin.Controllers
+{
+    [Authorize]
+    [Route("[controller]/[action]")]
+    public class ManageController : Controller
+    {
+        #region Index
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region User
+
+        [HttpGet]
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
