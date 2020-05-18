54e3dbe3295af149f23bd594bff0b7f4438047b0
D0683497 <ff129043923@gmail.com>
2020-05-19 06:47:16 +0800

    Rename Common To CommonViewModels And Add SelectItemViewModel

--- Komunumo/Komunumo.Admin/Models/CommonViewModels/PaginatedList.cs
+++ Komunumo/Komunumo.Admin/Models/CommonViewModels/PaginatedList.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace Komunumo.Admin.Models.Common
+namespace Komunumo.Admin.Models.CommonViewModels
 {
     public class PaginatedList<T> : List<T>
     {
